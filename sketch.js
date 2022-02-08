803b89d6cc313e716fbbbc0ec8568661a3eeadd2
SamruddhiBuchireddygari <98432566+SamruddhiBuchireddygari@users.noreply.github.com>
2022-02-08 17:53:48 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,57 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var box1, pig1;
+var backgroundImg,platform;
+var ball;
+var slingshot;
+var x,y;
+
+
+function preload() {
+    backgroundImg = loadImage("sprites/drivingrange.jpg");
+}
+
+function setup(){
+    var canvas = createCanvas(1200,400);
+    engine = Engine.create();
+    world = engine.world;
+
+
+  
+
+
+    ball1 = new BallClass(550,275,90,90);
+    slingshot = new SlingShot(ball1.body,{x:60,y:20});
+
+
+}
+
+function draw(){
+    background(backgroundImg);
+    Engine.update(engine);
+
+
+   ball1.display()
+   slingshot.display()
+
+  
+    }
+    function mouseDragged(){
+        Matter.Body.setPosition(ball1.body, {x: mouseX , y: mouseY});
+    }
+    
+    
+    function mouseReleased(){
+        slingshot.fly();
+        
+    }
+    
+    function keyPressed(){
+        if(keyCode === 32){
+            slingshot.attach(ball1.body);
+}
+    }
